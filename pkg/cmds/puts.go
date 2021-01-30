a411fb3c4ed96fc3f6edba6f1217444aebdaa029
stevenkl <stevenkl@rbx.run>
2021-01-30 01:55:39 +0100

    Command puts: now has option `-nonewline`

--- pkg/cmds/puts.go
+++ pkg/cmds/puts.go
@@ -2,14 +2,20 @@ package cmds
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/stevenkl/tcl.go/pkg/tcl"
 )
 
 // CommandPuts implements the `puts` command for tcl.
 func CommandPuts(i *tcl.Interp, argv []string, pd interface{}) (string, error) {
-	if len(argv) != 2 {
+	if len(argv) > 3 {
 		return "", fmt.Errorf("Wrong number of arguments for %s %s", argv[0], argv)
+	}
+	
+	if strings.ToLower(argv[1]) == "-nonewline" {
+		fmt.Print(argv[2])
+		return "", nil
 	}
 
 	fmt.Println(argv[1])
