5c1726579bb835006e21564762c465aee7d06cf1
stevenkl <stevenkl@rbx.run>
2021-02-10 22:34:25 +0100

    Make set command behaving more like the tcl one.

--- pkg/cmds/set.go
+++ pkg/cmds/set.go
@@ -6,11 +6,18 @@ import (
 
 // Set defines the `set` command
 func Set(i *tcl.Interp, argv []string, pd interface{}) (string, error) {
-	if len(argv) != 3 {
-		return "", ArityErr(i, argv[0], argv)
+	if len(argv) == 2 {
+		v, ok := i.Var(argv[1])
+		if !ok {
+			return "", nil
+		}
+		return string(v), nil
 	}
-	i.SetVar(argv[1], argv[2])
-	return argv[2], nil
+	if len(argv) == 3 {
+		i.SetVar(argv[1], argv[2])
+		return argv[2], nil
+	}
+	return "", ArityErr(i, argv[0], argv)
 }
 
 // Unset defines the `unset` command
